ad4672e953f581273ff27994bca02354adf619e9
ewbyf <ewbyf@umsystem.edu>
2025-03-30 22:55:33 -0500

    updated data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,30 +1,28 @@
 export const topics: string[] = ["animals", "fruits", "countries", "states"]
 
-export const animals: string[] = ["Lion", "Tiger", "Elephant", "Giraffe", 
-    "Zebra", "Monkey", "Bear", "Wolf", "Fox", "Deer", "Kangaroo", "Panda", 
-    "Koala", "Cheetah", "Leopard", "Rhino", "Hippo", "Alligator", "Crocodile", 
-    "Penguin", "Seal", "Walrus", "Dolphin", "Whale", "Shark", "Octopus", "Squid", 
-    "Jellyfish", "Starfish", "Crab", "Lobster", "Shrimp", "Clam", "Oyster",
-    "Turtle", "Tortoise", "Frog", "Toad", "Eagle", "Hawk", "Falcon", "Owl", 
-    "Parrot", "Tarantula", "Pigeon", "Seagull", "Flamingo", "Peacock", "Swan", 
+export const animals: string[] = ["Cat", "Dog", "Chicken", "Pig", "Cow", "Horse", "Lion", "Tiger", "Bird", "Goat", "Rabbit", "Squirrel", "Bat", "Elephant", "Giraffe", 
+    "Zebra", "Monkey", "Bear", "Wolf", "Fox", "Mouse", "Deer", "Hedgehog", "Kangaroo", "Panda", "Camel",
+    "Koala", "Cheetah", "Leopard", "Rhino", "Hippo", "Alligator", "Crocodile", "Capybara", 
+    "Penguin", "Seal", "Dolphin", "Whale", "Shark", "Octopus", "Squid", 
+    "Jellyfish", "Crab", "Lobster", "Shrimp", "Clam",
+    "Turtle", "Frog", "Owl", "Parrot", "Spider", "Eagle", "Flamingo", "Peacock", "Swan", 
     "Duck", "Goose", "Snake", "Lizard", "Chameleon", "Iguana",
-    "Scorpion", "Centipede", "Moth", "Ant", "Bee", "Butterfly", "Ladybug",
-    "Grasshopper", "Cricket", "Dragonfly", "Firefly", "Beetle", "Cockroach",
-    "Pufferfish", "Clownfish", "Seahorse", "Goldfish"]
+    "Scorpion", "Caterpillar", "Moth", "Ant", "Bee", "Butterfly", "Ladybug",
+    "Grasshopper", "Cricket", "Slug", "Wasp", "Beetle", "Cockroach",
+    "Pufferfish", "Goldfish"]
 
-export const fruits: string[] = ["Apple", "Banana", "Orange", "Grapes",
+export const fruits: string[] = ["Apple", "Banana", "Orange", "Grape",
     "Strawberry", "Blueberry", "Raspberry", "Blackberry", "Pineapple", 
     "Mango", "Papaya", "Kiwi", "Watermelon", "Cantaloupe", "Honeydew", 
     "Peach", "Plum", "Cherry", "Apricot", "Date", "Pomegranate", "Avocado", 
     "Coconut", "Lemon", "Lime", "Tangerine", "Dragonfruit", "Passionfruit", 
-    "Lychee", "Guava", "Persimmon", "Jackfruit", "Durian"]
+    "Lychee", "Guava", "Persimmon", "Durian"]
 
 export const countries: string[] = ["UnitedStates", "Canada", "Mexico", "UnitedKingdom",
     "Brazil", "Argentina", "Spain", "Portugal", "France", "Germany", "China", "India", "Japan", 
-    "NorthKorea", "SouthKorea", "Indonesia", "Singapore", "Thailand", "Vietnam", "Malayasia", 
-    "SouthAfrica", "Egypt", "Australia", "SaudiArabia", "Iran", "Iraq", "Turkey", "Russia", 
-    "Switzerland", "Finland", "Netherlands", "Nigeria", "ElSalvador", "Colombia", "Peru", "Panama", 
-    "Ukraine", "Italy", "Sweden", "Iceland", "Pakistan", "Chile", "Guatemala", "Honduras"]
+ "SouthKorea", "Indonesia", "Singapore", "Thailand", "Vietnam", "Malaysia", "Egypt", "Australia", "SaudiArabia", "Iran", "Iraq", "Turkey", "Russia", 
+    "Switzerland", "Finland", "Netherlands", "Nigeria", "Colombia", "Peru", "Ireland", 
+    "Ukraine", "Italy", "Sweden", "Iceland", "Pakistan", "Chile", "Guatemala", 'Taiwan', 'Romania', 'Israel', 'Philippines', 'Cambodia', 'Kenya', 'Bangladesh', 'Greece', 'Hungary' ]
 
 export const states: string[] = ["Alabama", "Alaska", "Arizona", "Arkansas",
     "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
