dce17f9670deda31bbdaeb44a90dfeb6ca4113cb
Linh <luongl7582@gmail.com>
2025-03-30 19:05:23 -0500

    we trying

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,15 +1,30 @@
-export const animals: string[] = [
-    
-]
+export const animals: string[] = ["Lion", "Tiger", "Elephant", "Giraffe", 
+    "Zebra", "Monkey", "Bear", "Wolf", "Fox", "Deer", "Kangaroo", "Panda", 
+    "Koala", "Cheetah", "Leopard", "Rhino", "Hippo", "Alligator", "Crocodile", 
+    "Penguin", "Seal", "Walrus", "Dolphin", "Whale", "Shark", "Octopus", "Squid", 
+    "Jellyfish", "Starfish", "Crab", "Lobster", "Shrimp", "Clam", "Oyster",
+    "Turtle", "Tortoise", "Frog", "Toad", "Eagle", "Hawk", "Falcon", "Owl", 
+    "Parrot", "Tarantula", "Pigeon", "Seagull", "Flamingo", "Peacock", "Swan", 
+    "Duck", "Goose", "Snake", "Lizard", "Chameleon", "Iguana",
+    "Scorpion", "Centipede", "Moth", "Ant", "Bee", "Butterfly", "Ladybug",
+    "Grasshopper", "Cricket", "Dragonfly", "Firefly", "Beetle", "Cockroach",
+    "Pufferfish", "Clownfish", "Seahorse", "Goldfish"]
 
-export const fruits: string[] = [
+export const fruits: string[] = ["Apple", "Banana", "Orange", "Grapes",
+    "Strawberry", "Blueberry", "Raspberry", "Blackberry", "Pineapple", 
+    "Mango", "Papaya", "Kiwi", "Watermelon", "Cantaloupe", "Honeydew", 
+    "Peach", "Plum", "Cherry", "Apricot", "Date", "Pomegranate", "Avocado", 
+    "Coconut", "Lemon", "Lime", "Tangerine", "dragonfruit", "Passionfruit", 
+    "Lychee", "Guava", "Persimmon", "Jackfruit", "Durian"]
 
-]
+export const countries: string[] = []
 
-export const countries: string[] = [
-
-]
-
-export const states: string[] = [
-
-]
+export const states: string[] = ["Alabama", "Alaska", "Arizona", "Arkansas",
+    "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
+    "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", 
+    "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",
+    "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
+    "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+    "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+    "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia",
+    "Washington", "West Virginia", "Wisconsin", "Wyoming"]
